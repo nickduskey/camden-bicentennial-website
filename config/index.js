8940981d6985d8c1771d1aa89b3bf0ac18fd985c
Nick Duskey <nickduskey@protonmail.com>
2018-03-14 20:00:25 -0400

    Fix path bug

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '',
+    assetsPublicPath: '/camden-bicentennial-website/dist/',
 
     /**
      * Source Maps
